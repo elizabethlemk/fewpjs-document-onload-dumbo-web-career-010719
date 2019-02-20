1c2b90fb0486fe9e922e3f8a5c7c84ad13a66b40
Elizabeth Le <elizabethlemk@yahoo.com>
2019-02-20 00:29:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const domContentLoaded = () => {
+  document.addEventListener("DOMContentLoaded", function(event) {
+    updateDOM()
+  });
+}
+
+const updateDOM = () => {
+  document.getElementById('text').innerHTML = "This is really cool!";
+}
